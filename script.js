9f25c3d30ceeb2fab7d13fa2debf2b1310616779
Anais <anaiszed447@gmail.com>
2022-09-06 22:10:32 -0400

    Created function which choooses math function

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2
 }
+
+//create new function that takes in 2 numbers and an operator
+//then calls one of the above functions
+
+function operate(num1, num2, operator){
+
+    switch(operator){
+
+        case "+":
+            add(num1,num2)
+        case "-":
+            subtract(num1,num2)
+        case "*":
+            multiply(num1,num2)
+        case "/":
+            divide(num1,num2)
+    }
+}
+
