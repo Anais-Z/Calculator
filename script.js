ecff42541bec7f11b0133c125f8b30cd28c539af
Anais <anaiszed447@gmail.com>
2022-09-06 22:03:51 -0400

    Created functions for basic arithmetic

---
+++ script.js
@@ -0,0 +1,17 @@
+//create function for add, subtract, multiply and divide
+
+function add(num1, num2){
+    return num1 + num2
+}
+
+function subtract(num1, num2){
+    return num1 - num2
+}
+
+function multiply(num1, num2){
+    return num1 * num2
+}
+
+function divide(num1, num2){
+    return num1 / num2
+}
