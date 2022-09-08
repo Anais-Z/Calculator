e983f9cf3d2cd004a48efaacd9e5e796cf9f5638
Anais <anaiszed447@gmail.com>
2022-09-08 09:49:12 -0400

    created logic to properly store the values

--- script.js
+++ script.js
@@ -16,13 +16,17 @@ function divide(num1, num2){
     return num1 / num2
 }
 
+//create array
+let values = []
+
+//used to concatenate strings 
+let currentNumElement = ''
+
 //create new function that takes in 2 numbers and an operator
 //then calls one of the above functions
-
 function operate(num1, num2, operator){
 
     switch(operator){
-
         case "+":
             add(num1,num2)
         case "-":
@@ -34,11 +38,71 @@ function operate(num1, num2, operator){
     }
 }
 
+//function for when a number button is clicked
 function numClicked(e){
+    counter = true
     let num = e.target.value
-    display.innerHTML += num
+    display.textContent += num
+    currentNumElement += num
+   // addElementToArray(num)
 }
 
+//function for when an operator button is clicked
+function operatorClicked(e){
+    let op  = e.target.value
+    display.textContent += op
+    addElementToArray(op)
+}
+
+
+//function will be used to store elements in the values array
+function addElementToArray(element){
+    
+        //convert type number then add it to string
+        values.push(Number(currentNumElement))
+
+        //then initialize to empty string
+        currentNumElement = ''
+
+        //then push operand
+        values.push(element)
+
+        console.log(values)
+    
+}
+
+
+
+function evaluate(){
+    //convert type number then add it to string
+    values.push(Number(currentNumElement))
+
+    //then initialize to empty string
+    currentNumElement = ''
+
+
+    console.log(values)
+
+    //get the three first elements in the array values 
+    //check if the even elements are numbers 
+    // check if the odd elements are string
+
+}
+
+//function for when the clear button is clicked
+function clearClicked(){
+   
+    //initialize var to empty string
+    currentNumElement = ''
+
+    //remove all elements from values array
+    values.splice(0, values.length)
+    console.log(values)
+
+    //clear the text on display element
+    display.textContent = ''
+
+}
 
 //get access to display element
 let display = document.querySelector('.display')
@@ -49,3 +113,20 @@ const numButtons = document.querySelectorAll('.num')
 numButtons.forEach( button => {
     button.addEventListener('click', numClicked)
 })
+
+//access equal button
+let equal = document.querySelector('.equal')
+
+equal.addEventListener('click', evaluate)
+
+//access the operator buttons and create event listener
+const operatorButtons = document.querySelectorAll('.operator')
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', operatorClicked)
+})
+
+//get access to clear button
+const clear = document.querySelector('.clear')
+
+clear.addEventListener('click', clearClicked)
